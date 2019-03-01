fda266603726b57d2a0a7f2b7c5bffe6d3525f63
julianajennifer <julianajennifer99@gmail.com>
2019-03-01 17:45:53 -0300

    Exemplos de variaveis

---
+++ variaveis/example.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let message = 'Hello Ju';
+let speak;
+speak = message;
+
+let age = 25;
+age = 15;
+
+let date = '14/03/2012';
+const myName = 'juliana';
+
+console.log(message);
+console.log(age);
+console.log(date);
+console.log(speak);
+console.log(myName);
