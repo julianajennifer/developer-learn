50adea7911e35653ec0499b55d45294c1e48251c
julianajennifer <julianajennifer99@gmail.com>
2019-03-01 17:44:48 -0300

    Adicionei exemplos com arquivos de tipo

---
+++ data-types/example.js
@@ -0,0 +1,22 @@
+'use strict';
+let age = 19;
+console.log(typeof age);
+
+let name = 'juli';
+console.log(typeof name);
+
+let isMan = false;
+console.log(typeof isMan);
+
+let no = null;
+console.log(typeof no);
+
+let variavel1;
+let variavel2 = variavel1;
+console.log(typeof variavel2);
+
+let people = {};
+console.log(typeof people);
+
+let s = Symbol();
+console.log(typeof s);
